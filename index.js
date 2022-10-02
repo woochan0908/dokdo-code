a0a032c898b72eb3c2116e273351dbd8260519e9
woochan0908 <100004579+woochan0908@users.noreply.github.com>
2022-10-02 11:57:59 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+//독도
+const Dokdo = require("dokdo");
+const Schema = require("./models/독도")
+
+client.on('messageCreate', async message => {
+    if (message.content == "??독도") {
+    const data = await Schema.find()
+    let ehrehuser = []
+    for (let i = 0; i < data.length; i++) {
+      ehrehuser.push(data[i].userid,);
+    }
+    const DokdoHandler = new Dokdo(client, {
+      aliases: ['독도', 'dok', 'dokdo', '서버', '독'], prefix: "??",
+      owners: ehrehuser,
+      noPerm: (message) => message.reply(":no_entry_sign: dokdo를 사용할 권한이 없습니다."),
+    });
+    await DokdoHandler.run(message);
+  }
+})
